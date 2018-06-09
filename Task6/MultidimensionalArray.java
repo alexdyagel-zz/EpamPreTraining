759485d16c9cb3eff5e1b988624f34a27dcfaa3f
alexdyagel <39384441+alexdyagel@users.noreply.github.com>
2018-06-09 21:28:03 +0300

    Add files via upload

--- Task6/MultidimensionalArray.java
+++ Task6/MultidimensionalArray.java
@@ -6,7 +6,7 @@ public class MultidimensionalArray {
     private static final int DEFAULT_SUM_VALUE = 0;
     private static final int ERROR_VALUE = -1;
     private static final int[] ERROR_INDEXES_OF_ARRAY = {-1, -1};
-    private static final int MIN_SIZE_OF_ARRAY_FOR_LOCAL_MINIMUM = 3;
+    private static final int MIN_SIZE_OF_ARRAY_FOR_EXTREMUM = 3;
     private static final int SECOND_ELEMENT_OF_ARRAY = 1;
 
 
@@ -105,12 +105,9 @@ public class MultidimensionalArray {
     /**
      * This method checks indexes of array, whether they are indexes of local minimum
      * Array's size should be not less than 3x3
-     * Value should be "inside" of array and have 8 neighbours
+     * Value should be "inside" of matrix and have 8 neighbours
      */
     private static boolean isLocalMinimum(double[][] array, int indI, int indJ) {
-        // local minimum in TwoDimensional array should have 8 neighbours
-        //at first check size and indexes
-        //if size and indexes are legal, compare value with all neighbour's values
         return areValidConditionsForExtremum(array, indI, indJ)
                 && array[indI][indJ] < array[indI - 1][indJ]
                 && array[indI][indJ] < array[indI + 1][indJ]
@@ -139,12 +136,9 @@ public class MultidimensionalArray {
     /**
      * This method checks indexes of array, whether they are indexes of local maximum
      * Array's size should be not less than 3x3
-     * Value should be "inside" of array and have 8 neighbours
+     * Value should be "inside" of matrix and have 8 neighbours
      */
     private static boolean isLocalMaximum(double[][] array, int indI, int indJ) {
-        // local maximum in TwoDimensional array should have 8 neighbours
-        //at first check size and indexes
-        //if size and indexes are legal, compare value with all neighbour's values
         return areValidConditionsForExtremum(array, indI, indJ)
                 && array[indI][indJ] > array[indI - 1][indJ]
                 && array[indI][indJ] > array[indI + 1][indJ]
@@ -161,8 +155,8 @@ public class MultidimensionalArray {
      * size of array shouldn't be less than 3x3 and values shouldn't be on the "border" of array
      */
     public static boolean areValidConditionsForExtremum(double[][] array, int indI, int indJ) {
-        return array.length >= MIN_SIZE_OF_ARRAY_FOR_LOCAL_MINIMUM
-                && array[0].length >= MIN_SIZE_OF_ARRAY_FOR_LOCAL_MINIMUM
+        return array.length >= MIN_SIZE_OF_ARRAY_FOR_EXTREMUM
+                && array[0].length >= MIN_SIZE_OF_ARRAY_FOR_EXTREMUM
                 && indI >= SECOND_ELEMENT_OF_ARRAY
                 && indJ >= SECOND_ELEMENT_OF_ARRAY
                 && indI < array.length - 1
