b454f7e53f29ea313d23e04f8023f4ae5704643c
alexdyagel <39384441+alexdyagel@users.noreply.github.com>
2018-06-12 00:44:44 +0300

    Add files via upload

--- Task6/Task06.java
+++ Task6/Task06.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Task06 {
 
     public static void main(String[] args) {
-        int n = 3;
-        int m = 10;
+        int n = 5;
+        int m = 5;
         double[][] array = new double[n][m];
         ArrayInitializer.randomInit(array, 0, 5);   // random initialization of array
         for (double internalArray[] : array) {
@@ -18,12 +18,15 @@ public class Task06 {
         System.out.printf("Geometric mean of array: %.2f\n", MultidimensionalArray.findGeometricMean(array));
         System.out.printf("Indexes of local minimum: %s\n", Arrays.toString(MultidimensionalArray.getIndexesOfLocalMinimum(array)));
         System.out.printf("Indexes of local maximum: %s\n", Arrays.toString(MultidimensionalArray.getIndexesOfLocalMaximum(array)));
-
-        double[][] transportedArray = MultidimensionalArray.transpose(array);
-        System.out.println("Transported array: ");
-        for (double internalArray[] : transportedArray) {
-            System.out.println(Arrays.toString(internalArray));
+        if (MultidimensionalArray.transpose(array)) {
+            System.out.println("Transported matrix: ");
+            for (double internalArray[] : array) {
+                System.out.println(Arrays.toString(internalArray));
+            }
+        } else{
+            System.out.println("matrix isn't square, so it cant be transposed");
         }
+
 
     }
 }
